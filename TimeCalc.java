c8df784429396c57a76daec4c769d9497a6f971e
נריה מאור <neri@nryhs-MacBook-Pro.local>
2025-11-11 11:55:21 +0200

    hw2-neryamaor

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
+        String FullTime = (args[0]);
+        String[] TimeParts = FullTime.split(":");
+        int Hours = Integer.parseInt(TimeParts[0]);
+        int Minutes = Integer.parseInt(TimeParts[1]);
+        int MinutesToAdd = Integer.parseInt(args[1]);
+        int TotalMinutes = Hours * 60 + Minutes + MinutesToAdd;
+        int TotalHours = TotalMinutes / 60 ;
+        int NewHours = TotalHours % 24;
+        int NewMinutes = TotalMinutes - TotalHours * 60;
+        String zero;
+        String zero2;
+        if(NewHours < 10 ){
+            zero = "0";
+        } else{zero = "";
+        }
+        if(NewMinutes<10){zero2 = "0";}
+        else{ zero2 = "";}
+        System.out.println( zero + NewHours + ":" + zero2 + NewMinutes);
 }
+}
+
